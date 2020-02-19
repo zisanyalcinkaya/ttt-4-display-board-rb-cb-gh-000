2707ad77080394aec76df02d5d7b096c0635599f
Zisan Yalcinkaya <zisan.yalcinkaya@bahcesehir.edu.tr>
2020-02-19 14:51:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
 # out the current state.	  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,5 +9,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
